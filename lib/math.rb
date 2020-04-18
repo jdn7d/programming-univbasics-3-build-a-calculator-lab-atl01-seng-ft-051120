637d814984236bf5fa07b616f13078408fa44aa9
Jessie Donovan <donovanajessie@gmail.com>
2020-04-18 20:13:21 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -8,7 +8,7 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
- num1 % num2
+ num1/num2
 end
 
 def multiplication(num1, num2)
@@ -16,7 +16,7 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
- num1 % num2 / 2
+ num1 % num2 
 end
 
 def square_root(num)
